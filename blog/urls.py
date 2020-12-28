bd838508f0de1b574e1fb2e98d6b7a5963b6317b
Ankit Kumar <anku.kr.1010@gmail.com>
2020-12-28 22:23:28 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path, include
+
+from blog import views
+
+app_name='blog'
+urlpatterns = [
+   
+    path('',views.all_blogs, name="all_blogs"),
+    path('<int:blog_id>/',views.detail, name="detail"),
+    
+
+]
